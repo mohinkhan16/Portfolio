afe9e3fdfa44b5b690e168d9a195d78720a9e299
MohinKhan <mohinpathan2004@gmail.com>
2026-05-19 11:58:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// =========================
+// ACTIVE NAVBAR LINK
+// =========================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach((section) => {
+
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.clientHeight;
+
+    if(scrollY >= sectionTop){
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach((link) => {
+
+    link.classList.remove("active");
+
+    if(link.getAttribute("href").includes(current)){
+      link.classList.add("active");
+    }
+
+  });
+
+});
+
+// =========================
+// SCROLL REVEAL ANIMATION
+// =========================
+
+const revealElements = document.querySelectorAll(
+  ".skills-card, .service-card, .about-card, .contact-box"
+);
+
+window.addEventListener("scroll", revealOnScroll);
+
+function revealOnScroll(){
+
+  const windowHeight = window.innerHeight;
+
+  revealElements.forEach((element) => {
+
+    const revealTop = element.getBoundingClientRect().top;
+
+    if(revealTop < windowHeight - 100){
+
+      element.style.opacity = "1";
+      element.style.transform = "translateY(0px)";
+
+    }
+
+  });
+
+}
+
+// Initial Style
+revealElements.forEach((element) => {
+
+  element.style.opacity = "0";
+  element.style.transform = "translateY(60px)";
+  element.style.transition = "all 0.8s ease";
+
+});
+
